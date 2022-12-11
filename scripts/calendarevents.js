7cbf179b49545f5556831ba19aac4cc2bf374afa
Shamil Jakeev <shamil.dzhakeev@ru.axxonsoft.com>
2022-12-11 23:41:02 +0500

    Add pre-created web-pages by GenoPro builder

---
+++ scripts/calendarevents.js
@@ -0,0 +1,11 @@
+months=['','January','February','March','April','May','June','July','August','September','October','November','December'];
+weekdays=['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+SpecialDay=1;
+oDic = {
+'view':'View',
+'changes':'Apply changes',
+'print':'Printer friendly',
+'export':'Export events displayed',
+'filter':'Filter events',
+'jump':'Jump to month'}
+
